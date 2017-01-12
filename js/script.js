58afa10a7ef3a2ed6402203c22d308d647e232cf
jmarsh <j.marshmoto@gmail.com>
2017-01-12 20:52:37 +0000

    Day 10 #100Days

--- js/script.js
+++ js/script.js
@@ -7,18 +7,35 @@ function main(){
 
 	var quoteButton = document.querySelector("#quoteButton");
 	var quote = document.querySelector("#quote");
-	
-	$.getJSON("https://got-quotes.herokuapp.com/quotes", function(json) {	
-		console.log(json);
-		quote.innerHTML = "\"" + json.quote + "\"" + "<br>" + "- " + json.character;
-	}, 'jsonp');
+	getQuote();
 
-	quoteButton.addEventListener("click", function(){
+	function twitter(param){
+			var url = "https://twitter.com/intent/tweet/?text="
+			document.querySelector("a").setAttribute("href", url + param);
+			console.log(param);	
+	};
+	
+	function getQuote(){
 		$.getJSON("https://got-quotes.herokuapp.com/quotes", function(json) {	
 			console.log(json);
 			quote.innerHTML = "\"" + json.quote + "\"" + "<br>" + "- " + json.character;
+			//onlick here - needs more work
+			twitterButton.addEventListener("click", function(){
+				if(quote.textContent.length < 140){
+					twitter(quote.textContent);
+				};
+				return;
+			});
 		}, 'jsonp');
+	};
+
+	
+
+	quoteButton.addEventListener("click", function(){
+		//DRY - removed the getJSON request as the same
+		getQuote();
 	});
+
 
 }
 
--- to_do.md
+++ to_do.md
@@ -2,8 +2,9 @@ choose API - done
 Fetch data - done
 Add a random quote to an HTML element - done
 Button to pick another quote - done
-Twitter button
+Twitter button - done
+If/else for quote length - if over 140 then disable twitter button OR only display tweest < 140
 Change background colour on click
 
 
-Create a well or Jumbotron to change the quotes
+Create a well or Jumbotron to change the quotes - done
