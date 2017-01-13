b4a4aecb37876a3b3e20f01b0caa34633d018b5d
jmarsh <j.marshmoto@gmail.com>
2017-01-13 09:12:36 +0000

    Day 11 #100days

--- js/script.js
+++ js/script.js
@@ -1,42 +1,50 @@
-
-console.log("hello world");
-
-
-
 function main(){
 
 	var quoteButton = document.querySelector("#quoteButton");
 	var quote = document.querySelector("#quote");
+	var buttonArray = ["primary", "success", "warning", "info"];
+	var color = 0;
 	getQuote();
 
 	function twitter(param){
 			var url = "https://twitter.com/intent/tweet/?text="
 			document.querySelector("a").setAttribute("href", url + param);
 			console.log(param);	
-	};
+	}; //twitter
 	
 	function getQuote(){
 		$.getJSON("https://got-quotes.herokuapp.com/quotes", function(json) {	
 			console.log(json);
-			quote.innerHTML = "\"" + json.quote + "\"" + "<br>" + "- " + json.character;
-			//onlick here - needs more work
+			var quoteBody = json.quote;
+			var character = json.character;
+			//only get quotes of tweetable length
+			if((quoteBody.length+character.length) > 140){
+				return getQuote();
+			} else {
+				quote.innerHTML = "\"" + quoteBody + "\"" + "<br>" + "- " + character;
+			};
 			twitterButton.addEventListener("click", function(){
 				if(quote.textContent.length < 140){
 					twitter(quote.textContent);
 				};
 				return;
-			});
-		}, 'jsonp');
-	};
-
-	
+			}); //twitterButton
+		}, 'jsonp'); //getJSON
+	}; //getQuote
 
 	quoteButton.addEventListener("click", function(){
-		//DRY - removed the getJSON request as the same
 		getQuote();
-	});
-
-
+		//change button color
+		var buttons = document.querySelectorAll("button");
+		if(color < 3){
+			color++
+		} else {
+			color = 0;
+		}
+		for(var i = 0; i < 2; i++){
+			buttons[i].setAttribute("class", "btn btn-" + buttonArray[color]);
+		};
+	});//quoteButton
 }
 
 $(document).ready(main());
--- to_do.md
+++ to_do.md
@@ -4,7 +4,8 @@ Add a random quote to an HTML element - done
 Button to pick another quote - done
 Twitter button - done
 If/else for quote length - if over 140 then disable twitter button OR only display tweest < 140
-Change background colour on click
+Change button colour on click - done
+Twiiter icon
 
 
 Create a well or Jumbotron to change the quotes - done
